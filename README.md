0969d7ea745010549ebe7bd9a04eb3f8e0a5523b
Atte Kivimaki <aj.kivimaki@gmail.com>
2024-01-28 16:44:45 +0200

    tutorial step 1-5

---
+++ README.md
@@ -0,0 +1,11 @@
+# Open AI project
+
+Tutorial:
+
+https://www.youtube.com/playlist?list=PL4cUxeGkcC9ipdXMDVcGimIVMG_Z6-Vsu
+
+Generate:
+
+- description of YouTube video
+- tags
+- image / thumbnail
---
+++ controllers/openaiController.js
@@ -0,0 +1,48 @@
+const openai = require("../config/openaiConfig");
+
+const generateMeta = async (req, res) => {
+  const { title } = req.body;
+
+  const description = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: `Come up with a description for YouTube video called ${title}`,
+      },
+    ],
+    max_tokens: 100,
+  });
+
+  const tags = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: `Come up with 10 keywords for a YouTube video called ${title}`,
+      },
+    ],
+    max_tokens: 100,
+  });
+
+  res.status(200).json({
+    description: description.choices[0].message,
+    tags: tags.choices[0].message,
+  });
+};
+
+const generateImage = async (req, res) => {
+  const image = await openai.images.generate({
+    model: "dall-e-3",
+    prompt: req.body.prompt,
+    size: "1024x1024",
+    quality: "standard",
+    n: 1,
+  });
+
+  res.json({
+    url: image.data[0].url,
+  });
+};
+
+module.exports = { generateMeta, generateImage };
